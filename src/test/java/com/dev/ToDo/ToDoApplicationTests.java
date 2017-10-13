34d981021965e667199e4f598966a75bbd84641e
tamerrab2003 <tamerrab2003@gmail.com>
2017-10-14 01:53:51 +0200

    updating test case

--- src/test/java/com/dev/ToDo/ToDoApplicationTests.java
+++ src/test/java/com/dev/ToDo/ToDoApplicationTests.java
@@ -1,16 +1,26 @@
 package com.dev.ToDo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class ToDoApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void findAllTest() {
+		String body = this.restTemplate.getForObject("/api/items", String.class);
+		assertThat(body).isNotEmpty();
 	}
 
 }
